877c3193a5bffe8aeaf230766b0dbc7333de7140
Gitbuster1 <tiberiug2010@gmail.com>
2023-09-12 14:19:08 +0300

    Added the Vite React client app

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import PostsList from './features/posts/PostsList';
+
+function App() {
+  return (
+      <>
+          <div className="app">
+              <h1>React App</h1>
+              <p>Test</p>
+              <PostsList />
+          </div>
+      </>
+  );
+}
+
+export default App
